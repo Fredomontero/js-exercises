ea9bf40b3ba63970f9df1c2e7c72782e56dc43cf
Fredomontero <christianmontero08@gmail.com>
2020-11-18 17:21:50 -0800

    Solution for exercise #5

--- src/exe_4/exe_4.js
+++ src/exe_4/exe_4.js
@@ -1,9 +1,9 @@
 // const smallArray = [1,2,3,4]
 // const largeArray = [1,2,3,4].concat(new Array(smallArray.length));
-// const smallArray = [0,1,2,3]
-// const largeArray = [1,1,1,1].concat(new Array(smallArray.length));
-const largeArray = [1,3,5,7,9].concat(new Array(5));
-const smallArray = [0,2,4,6,8];
+const smallArray = [0,1,2,3]
+const largeArray = [1,1,1,1].concat(new Array(smallArray.length));
+// const largeArray = [1,3,5,7,9].concat(new Array(5));
+// const smallArray = [0,2,4,6,8];
 const largeArraySize = largeArray.length;
 
 const mergeArrays = (largeArray, smallArray) => {
@@ -18,7 +18,7 @@ const mergeArrays = (largeArray, smallArray) => {
   }
 }
 
-module.exports = mergeArrays;
+// module.exports = mergeArrays;
 
 mergeArrays(largeArray, smallArray);
 console.log(largeArray); // Expecting: [0,1,2,3,4,5,6,7,8,9]
--- src/exe_5/exe_5.js
+++ src/exe_5/exe_5.js
@@ -1,6 +1,30 @@
 const arr = [0,1,2,3,4,5,6,7,8,9];
 const blockSize = 3;
-reverseBlocks(arr, blockSize);
+
+/**
+ * Function to reverse by block of length blockSize an array of numbers
+ * 
+ * @param {Array} arr - array of numbers to reverse by blocks
+ * @param {Number} blockSize - length of the block
+ */
+const reverseBlocks = (arr, blockSize) => {
+  let queues = [];
+  for(let i = 0; i<arr.length; i++){
+    let index = i/blockSize >> 0;
+    if(queues[index] === undefined) queues[index] = [];
+    queues[index].push(arr[i]);
+  }
+  for(let i = 0; i<arr.length; i++){
+    let index = i/blockSize >> 0;
+    arr[i] = queues[index].pop();
+  }
+}
+
+module.exports = reverseBlocks;
+
+// console.log(arr);
+// reverseBlocks(arr, blockSize);
+// console.log(arr);
 /**
 * Expected result:
 * [2,1,0,5,4,3,8,7,6,9]
