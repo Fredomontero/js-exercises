edd110956dbe5c9c3138556aec315c01850de509
Fredomontero <christianmontero08@gmail.com>
2020-11-26 11:12:51 -0800

    refactor exercise #8

--- src/exe_8/exe_8_functional.js
+++ src/exe_8/exe_8_functional.js
@@ -4,38 +4,16 @@
   * @param {Object} oldObject - Object to be flatten
   * @param {String} parentName - Name of the parent
 */
-const flattenFunctional = (oldObject, parentName) => iterateObjectProperties(oldObject, parentName)
+const flattenFunctional = (oldObject, parentName) => Object.entries(oldObject).reduce((accumulator, current) => {
+  if(isObject(current[1])){
+      let innerObject = flattenFunctional(current[1], parentName + '_' + current[0]);
+      for(const property in innerObject)
+          accumulator[property] = innerObject[property]
+  }else accumulator[`${parentName}_${current[0]}`] = current[1];
+  return accumulator;
+}, {})
 
-/**
- * Function that iterates through an object properties
- * 
- * @param {Object} object - object to iterate 
- * @param {String} parentName - name of the parent object
- */
-const iterateObjectProperties = (oldObject, parentName) => {
-  const sourceObject = Object.assign({}, oldObject)
-  let flatObject = {};
-  for (let property in sourceObject) {
-    if (objectContainsProperty(sourceObject, property)) {
-      if (isObject(sourceObject[property])) {
-        let innerObject = flattenFunctional(sourceObject[property], parentName + '_' + property);
-        for(let prop in innerObject)
-          if(objectContainsProperty(innerObject, prop)) flatObject[prop] = innerObject[prop];
-      } else {
-          flatObject[parentName+"_"+property] =  sourceObject[property];
-      }
-    }
-  }
-  return Object.assign({}, flatObject);
-};
 
-/**
- * Function that verifies if the Object passed in the object argument has the property passed in the property argument
- * 
- * @param {Object} object - Object where property is going to be searched
- * @param {Object|Number|String|[]} property - property to verify in object
- */
-const objectContainsProperty = (object, property) => object.hasOwnProperty(property)
 
 /**
  * Function that verifies the type of the object passed as an argument, if it's an object but not an Array return true otherwise returns false
--- src/exe_8/sandbox.js
+++ src/exe_8/sandbox.js
@@ -14,4 +14,17 @@ const data = {
   }
 };
 
-Object.keys(data).map(item => console.log(data[item]));
+const flatten = (oldObject, parentName) => Object.entries(oldObject).reduce((accumulator, current) => {
+    if(isObject(current[1])){
+        let innerObject = flatten(current[1], parentName + '_' + current[0]);
+        for(const property in innerObject)
+            accumulator[property] = innerObject[property]
+    }else accumulator[`${parentName}_${current[0]}`] = current[1];
+    return accumulator;
+}, {})
+
+const isObject = (object) =>  typeof object === "object" && !(object instanceof Array)
+
+let result = flatten(data, 'data');
+console.log("RESULT");
+console.log(result);
