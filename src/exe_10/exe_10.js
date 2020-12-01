fbecc449cdbfa5bfafb43a121d2dbc482c76376f
Fredomontero <christianmontero08@gmail.com>
2020-11-30 22:11:00 -0800

    Exercise #10

---
+++ src/exe_10/exe_10.js
@@ -0,0 +1,25 @@
+// const bTree = '(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))';
+const bTree = '(A,(B),(C))';
+
+// (A,(B),(C)) -> 0,1,2
+// (A,(B,(D),(E)),(C)) -> 0,1,3,4,2
+
+
+/**
+* @param {String} tree  
+* @param {String} order  'infix' (default) | 'prefix' | 'postfix'
+*/
+function printTree(tree, order) {
+  let current = [];
+  let index = 0;
+  let open = true;
+  for(let i = 1; i < tree.length; i++){
+    
+  }
+}
+
+const BinaryTreeNode = (value) => {
+  value;
+  left = null;
+  right = null;
+}
