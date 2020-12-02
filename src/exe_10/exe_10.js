bbeef23d6bcf5573f7ba738f1c71f7d94cb17511
Fredomontero <christianmontero08@gmail.com>
2020-12-01 21:26:22 -0800

    Document exe_10 with JSDocs

--- src/exe_10/exe_10.js
+++ src/exe_10/exe_10.js
@@ -7,6 +7,55 @@ const bTree = '(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))';
 * @param {String} order  'infix' (default) | 'prefix' | 'postfix'
 */
 function printTree(tree, order) {
+
+  let binaryTree = buildTree(tree);
+  // printPrefix(binaryTree);
+  // printPostfix(binaryTree);
+  printInfix(binaryTree);
+
+}
+
+const printPrefix = (node) => {
+  const traversePrefix = (node) => {
+    if(!node) return;
+    console.log(node.data);
+    traversePrefix(node.left);
+    traversePrefix(node.right);
+  }
+  traversePrefix(node);
+}
+
+const printPostfix = (node) => {
+  const traversePostfix = (node) => {
+    if(node.left) traversePostfix(node.left);
+    if(node.right) traversePostfix(node.right)
+    console.log(node.data);
+  }
+  traversePostfix(node);
+}
+
+const printInfix = (node) => {
+  let queue = [];
+  if(!node) return;
+  queue.push(node);
+  while(queue.length){
+    let temp = queue.shift();
+    console.log(temp.data);
+    if(temp.left) queue.push(temp.left);
+    if(temp.right) queue.push(temp.right);
+  }
+
+}
+
+class TreeNode{
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function buildTree(tree) {
   
   let index = 0;
   let regex = /[A-Za-z0-9]/;
@@ -47,21 +96,9 @@ function printTree(tree, order) {
     }
 
     return node;
-
   }
 
-  let binaryTree = buildTreeNode(tree);
-  console.log("The tree is: ");
-  console.log(binaryTree);
-
-}
-
-class TreeNode{
-  constructor(data){
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
+  return buildTreeNode(tree);
 }
 
 printTree(bTree);
