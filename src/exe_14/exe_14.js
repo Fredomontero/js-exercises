41a4e25d771e72bd6c75982fcd3548863325e0cc
Fredomontero <christianmontero08@gmail.com>
2020-12-14 21:49:21 -0800

    Document code with JSDocs

--- src/exe_14/exe_14.js
+++ src/exe_14/exe_14.js
@@ -10,6 +10,11 @@ let matrix = [
   [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0]
 ];
 
+/**
+ * Function that returns the greatest area formed by rectangles of 1's
+ * 
+ * @param {Array} matrix - Binary Matrix
+ */
 const greatestArea = (matrix) => {
   let state = [];
   let area = 0;
@@ -26,6 +31,11 @@ const greatestArea = (matrix) => {
   return maxArea;
 }
 
+/**
+ * Function that calculates the largest reactangle area by row
+ * 
+ * @param {Array} heights - Array of sum of consecutives 1's per row
+ */
 const largestAreaPerRow = (heights) => {
   const n = heights.length;
   let stack = [];
@@ -48,6 +58,12 @@ const largestAreaPerRow = (heights) => {
   return maxArea;
 };
 
+/**
+ * 
+ * @param {Number} index - reference index to calculate the width
+ * @param {Array} heights - Array with sum of consecutive 1's
+ * @param {Array} stack - Stack of indexes
+ */
 const calculateArea = (index, heights, stack) => {
   let height = heights[stack.pop()];
   let width = (stack.length > 0) ? index - stack[stack.length-1] - 1 : index;
