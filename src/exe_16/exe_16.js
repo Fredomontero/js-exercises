3089e9912592d7292d5f2127569403b510ea017a
Fredomontero <camontero@tekmexico.com>
2020-12-17 11:15:00 -0800

    Refactor execise #16

--- src/exe_16/exe_16.js
+++ src/exe_16/exe_16.js
@@ -19,17 +19,26 @@
 // node6.next = node7;
 // node7.next = node8;
 // node8.next = node9;
-// node9.next = node4;
+// node9.next = node7;
 
 // let list = new LinkedList(node1);
 
 const findStartingLoopNode = (node) => {
-  let cache = [];
-  while(node){
-    if(cache.includes(node)) return node;
-    cache.push(node);
-    node = node.next;
+  let ptr1 = node;
+  let ptr2 = node;
+  while(ptr1 && ptr2 && ptr2.next){
+    ptr1 = ptr1.next;
+    ptr2 = ptr2.next.next;
+    if(ptr1 === ptr2){
+      ptr1 = node;
+      while(ptr1 !== ptr2){
+        ptr1 = ptr1.next;
+        ptr2 = ptr2.next;
+      }
+      return ptr2;
+    }
   }
+  return null;
 }
 
 module.exports = findStartingLoopNode;
