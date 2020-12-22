356061eada9690f4ac4ed6fc86db70e119c22e8b
Fredomontero <christianmontero08@gmail.com>
2020-12-21 16:32:00 -0800

    code for exercise #18 and tests

---
+++ src/exe_18/exe_18.js
@@ -0,0 +1,22 @@
+/**
+ * Function for deep object assingnment  
+ * 
+ * @param {Object} obj - Object to add a new property
+ * @param {String} path - Path to insert new property
+ * @param {*} value - Value to assign to the nwe property
+ */
+const set = (obj, path, value) => {
+  let index = 0;
+  let properties = path.split(".");
+  
+  const assign = (currentObject, index) => {
+    if(!currentObject.hasOwnProperty([properties[index]])) currentObject[properties[index]] = {};
+    if(properties[index + 1]) assign(currentObject[properties[index]], index + 1);
+    else{
+      currentObject[properties[index]] = value;
+    }
+  }
+  assign(obj, index);
+}
+
+module.exports = set;
---
+++ src/exe_18/exe_18.test.js
@@ -0,0 +1,42 @@
+const { expect } = require('@jest/globals');
+const set = require('./exe_18');
+
+test("should assign 29 to user.age", () => {
+  const user = {
+    id: 101,
+    name: "Chris"
+  }
+
+  set(user, 'age', 29);
+  expect(user.age).toBe(29);
+});
+
+test("should assign 'Christian' to user.personalInfo.name", () => {
+  const user = {
+    id: 101,
+  }
+
+  set(user, 'personalInfo.name', "Christian");
+  expect(user.personalInfo.name).toBe("Christian");
+});
+
+test("should assign 'Alicia Maria' to user.personalInfo.address.street", () => {
+  const user = {
+    id: 101,
+  }
+
+  set(user, 'personalInfo.address.street', "Alicia Maria");
+  expect(user.personalInfo.address.street).toBe("Alicia Maria");
+});
+
+test("should modify value of user.personalInfo.name to 'Alfredo'", () => {
+  const user = {
+    id: 101,
+    personalInfo: {
+      name: "Christian"
+    }
+  }
+
+  set(user, 'personalInfo.name', "Alfredo");
+  expect(user.personalInfo.name).toBe("Alfredo");
+});
