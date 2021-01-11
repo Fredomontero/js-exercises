88e3a93b6c64ae9331fc18105b09c56dcd53f16f
Fredomontero <christianmontero08@gmail.com>
2021-01-10 23:16:09 -0800

    Refactor code for exercise #19

--- src/exe_19/nb-test.js
+++ src/exe_19/nb-test.js
@@ -9,36 +9,27 @@
     return el;
   }
 
-  function assert (pass, message){
-    console.log(`For ${message} the root is: `, this.root);
-    return root.appendChild(result(message,pass))
-  };
+  const assert = (pass, message) => root.appendChild(result(message,pass));
 
   function test(description, testBlock){
     const parent = root;
     root = assert(undefined, description).appendChild(document.createElement('ul'));
-    (function f(root){
-      console.log(root)
-      testBlock.apply({root});
-    })(root);
+    testBlock();
     root = parent;
   }
 
   const nativeSetTimeout = window.setTimeout;
 
   const setTimeout = function(cb, delay){
-    console.log("[1] ROOT: ", root);
     var myArgs = Array.prototype.slice.call(arguments, 2);
-    (function (root){
-      return nativeSetTimeout(cb instanceof Function ? function () {
-        //Here I need to have my root
-        // root = document.getElementById('test');
-        console.log("[2] ROOT: ", root);
-        console.log("CB: ", cb);
-        let _this = {root}
-        cb.call(_this, myArgs);
-      } : cb, delay)
-    })(root)
+    function customCB(customRoot){
+      return function(){
+        root = customRoot;
+        cb(...myArgs);
+        root = parent;
+      };
+    }
+    nativeSetTimeout(customCB(root), delay);
   };
 
   global.setTimeout = setTimeout;
